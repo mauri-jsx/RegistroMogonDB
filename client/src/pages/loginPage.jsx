9f81975cb6cce772e739adab9c747d8130866553
Maurcio Heredia <maurih46322945@gmail.com>
2024-08-30 12:21:48 -0300

    Se creo el longin

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -1,10 +1,45 @@
-
+import { useForm } from "react-hook-form";
 function LoginPage() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            {...register("email", { required: "Email es requerido" })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Email"
+          />
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+
+          <input
+            type="password"
+            {...register("password", {
+              required: "La contraseña es requerida",
+            })}
+            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
